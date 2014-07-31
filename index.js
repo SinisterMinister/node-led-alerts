718990d212dba9a45371c77ad8c4a9a544271de9
Codey Whitt <codey.whitt@pearson.com>
2014-07-31 14:44:13 -0600

    Working on it

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function draw () {
 		frameRate = AnimationLoop.getFrameRate().toString()
 	}
 
-	ctx.fillStyle = 'white';
+	ctx.fillStyle = 'blue';
 	ctx.fillRect(0, 0, w, h);
 
-	ctx.font = "16px sans-serif";
+	ctx.font = "12px sans-serif";
 
 	var gradient=ctx.createLinearGradient(0,0,w,0);
 	gradient.addColorStop("0","magenta");
@@ -25,18 +25,10 @@ function draw () {
 
 	// Fill with gradient
 	ctx.fillStyle = gradient;
-	ctx.fillText("FPS: "+frameRate, 10, 20);
-	ctx.fillText("Frame: "+frame.toString(), 10, 40);
+	ctx.fillText("FPS: "+frameRate, 10, 14);
+	ctx.fillText("Frame: "+frame.toString(), 10, 30);
 
 	var data = ctx.getImageData(0, 0, w, h).data;
-
-	var pdata = [];
-
-	for (var i = 0; i < 32; i++) {
-		pdata.push(data[i]);
-	}
-
-	console.info.apply(null, pdata);
 }
 
 AnimationLoop.register('canvasDrawer', draw);
