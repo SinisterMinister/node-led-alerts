4d6e4b90061784945baba6825477ae931d8a4d3f
Codey Whitt <codey.whitt@pearson.com>
2014-07-31 13:10:17 -0600

    Working on it

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ function draw () {
 	ctx.fillStyle = gradient;
 	ctx.fillText("FPS: "+frameRate, 10, 20);
 	ctx.fillText("Frame: "+frame.toString(), 10, 40);
+
+	var data = ctx.getImageData(0, 0, w, h);
+	console.info(data);
 }
 
 AnimationLoop.register('canvasDrawer', draw);
