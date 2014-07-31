317a3f5b5011d56a97117b940b89f5d24962aeea
Codey Whitt <codey.whitt@pearson.com>
2014-07-31 12:45:55 -0600

    Working on it

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var Canvas = require('openvg-canvas'),
 	AnimationLoop = require('./lib/animation-loop'),
     canvas = new Canvas(128, 32),
     ctx = canvas.getContext('2d'),
-    w = canvas.width, h = canvas.height,
-	frameRate = 0;
+    w = canvas.width, h = canvas.height;
 
 
 
@@ -12,13 +11,13 @@ function draw () {
 
 	ctx.font = "16px sans-serif";
 
-	var gradient=ctx.createLinearGradient(0,0,w,0);
-	gradient.addColorStop("0","magenta");
-	gradient.addColorStop("0.5","blue");
-	gradient.addColorStop("1.0","red");
-
-	// Fill with gradient
-	ctx.fillStyle = gradient;
+//	var gradient=ctx.createLinearGradient(0,0,w,0);
+//	gradient.addColorStop("0","magenta");
+//	gradient.addColorStop("0.5","blue");
+//	gradient.addColorStop("1.0","red");
+//
+//	// Fill with gradient
+//	ctx.fillStyle = gradient;
 	ctx.fillText(AnimationLoop.getFrameRate(), 10, 10);
 }
 
