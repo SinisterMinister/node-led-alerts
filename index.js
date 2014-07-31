bd248e9b1673a4c076a3d82cd8ecc02de812e7f0
Codey Whitt <codey.whitt@pearson.com>
2014-07-31 11:26:03 -0600

    Committing test code

---
+++ index.js
@@ -0,0 +1,26 @@
+var Canvas = require('openvg-canvas'),
+	AnimationLoop = require('./lib/animation-loop'),
+    canvas = new Canvas(128, 32),
+    ctx = canvas.getContext('2d'),
+    w = canvas.width, h = canvas.height,
+	frameRate = 0;
+
+
+
+function draw () {
+	ctx.clearRect(0, 0, w, h);
+
+	ctx.font = "16px sans-serif";
+
+	var gradient=ctx.createLinearGradient(0,0,w,0);
+	gradient.addColorStop("0","magenta");
+	gradient.addColorStop("0.5","blue");
+	gradient.addColorStop("1.0","red");
+
+	// Fill with gradient
+	ctx.fillStyle = gradient;
+	ctx.fillText(AnimationLoop.getFrameRate(), 10, 10);
+}
+
+AnimationLoop.register('canvasDrawer', draw);
+AnimationLoop.start();
