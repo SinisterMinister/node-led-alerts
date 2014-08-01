5dfe936cfef06b51f0727e41af8936f074fa0721
Ritz, Ryan W <Ryan.Ritz@pearson.com>
2014-08-01 12:04:22 -0600

    Hapi Server

--- index.js
+++ index.js
@@ -61,3 +61,45 @@ AnimationLoop.register('canvasDrawer', draw);
 
 // Start the animation loop
 AnimationLoop.start();
+
+
+// HAPI server
+
+var Hapi    = require('hapi'); 		// call express
+var server = Hapi.createServer('0.0.0.0', parseInt(process.env.PORT, 10) || 3000);
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+server.route({
+  method: 'GET'
+, path: '/'
+, handler: function(req, reply) {
+    reply('Server is running!');
+  }
+});
+
+
+server.route({
+  method: 'POST'
+, path: '/message'
+, config: {
+    handler: function(req, reply) {
+      var message = {
+        status: req.payload.status
+      , text: req.payload.text
+      , system: req.payload.system
+      };
+      var status = req.payload.status;
+      var system = req.payload.system;
+      var text = req.payload.text;
+      //quotes.push(newQuote);
+      console.log("Message received:");
+      console.log("Text: " + text);
+      console.log("Status: " + status);
+      console.log("System: " + system);
+      // Call node-led-alerts here
+      //node-led-alerts.draw(status, text);
+    }
+  }
+});
+	
+server.start();
